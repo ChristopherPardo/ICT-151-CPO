8271577affebebc8e949bf3ee5a5b2e405235b2d
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-02-06 15:55:13 +0100

    Connection to DataBase

--- index.php
+++ index.php
@@ -4,3 +4,19 @@
   Project : Test ICT-151
   Date : 06.02.2020
 */
+
+$user = "phpApplication";
+$pass = 'Pa$$w0rd';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=MCU', $user, $pass);
+    foreach($dbh->query('SELECT * from actors') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
