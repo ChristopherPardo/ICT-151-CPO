642b630656c2568e7b0b676b737fe42b472eed01
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-02-07 09:37:16 +0100

    Ajout de recuperation d'un objet de la DataBase

--- index.php
+++ index.php
@@ -1,22 +1,55 @@
 <?php
 /**
-  Author : Christopher Pardo
-  Project : Test ICT-151
-  Date : 06.02.2020
-*/
+ * Author : Christopher Pardo
+ * Project : Test ICT-151
+ * Date : 06.02.2020
+ */
 
-$user = "phpApplication";
-$pass = 'Pa$$w0rd';
+function getAllItems($table)
+{
+    require ".const.php";
 
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=MCU', $user, $pass);
-    foreach($dbh->query('SELECT * from actors') as $row) {
-        print_r($row);
+    try {
+        $dbh = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $user, $pass);
+        $query = 'SELECT * FROM '.$table;
+        $statment = $dbh->prepare($query); //Prepare query
+        $statment->execute(); //Execute query
+        $queryResult = $statment->fetchAll(); //Prepare result for client
+        return $queryResult;
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
     }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
 }
+
+function getAnItem($table, $id)
+{
+    require ".const.php";
+
+    try {
+        $dbh = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $user, $pass);
+        $query = 'SELECT * FROM '.$table.' WHERE id = '.$id;
+        $statment = $dbh->prepare($query); //Prepare query
+        $statment->execute(); //Execute query
+        $queryResult = $statment->fetch(); //Prepare result for client
+        return $queryResult;
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+}
+
+// Test unitaire de la fonction getAllItems
+/*$items = getAllItems("heroes");
+var_dump($items);*/
+
+// Test unitaire de la fonction getAItem
+$items = getAnItem("filmmakers", "4");
+//var_dump($items);
+echo "Cette acteur s'appelle ".$items['firstname']." ".$items['lastname'];
+
+
 
 ?>
